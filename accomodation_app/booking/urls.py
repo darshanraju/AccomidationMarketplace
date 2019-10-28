ecb2fce2912e4975970875f3db098f346788f863
derje <j.der@student.unsw.edu.au>
2019-10-28 12:35:08 +1100

    added api to get all bookings for a property

--- accomodation_app/booking/urls.py
+++ accomodation_app/booking/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from django.conf.urls import url
-from .api import BookingAPI, MakeBookingAPI, UpdateBookingAPI
+from .api import BookingAPI, MakeBookingAPI, UpdateBookingAPI, GetAllBookingsAPI
 
 urlpatterns = [
     path('api/booking/add', MakeBookingAPI.as_view()),
     url(r'^api/booking/(?P<id>\d+)$', BookingAPI.as_view()),
-    url(r'^api/booking/update/(?P<id>\d+)$', UpdateBookingAPI.as_view())
+    url(r'^api/booking/update/(?P<id>\d+)$', UpdateBookingAPI.as_view()),
+    url(r'^api/booking/for/(?P<property_id>\d+)$', GetAllBookingsAPI.as_view())
 ]
