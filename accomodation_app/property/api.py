b0daae1a200781c51c1330bbcf6b0c01805a9d05
derje <j.der@student.unsw.edu.au>
2019-11-03 14:35:18 +1100

    search based on price, check-in, check-out, suburb, post_code passed in through url querry string bassed of this link <https://wsvincent.com/django-search/>

--- accomodation_app/property/api.py
+++ accomodation_app/property/api.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions, mixins
 from rest_framework.response import Response
 from .models import Property
+from booking.models import Booking
 from .serializers import PropertySerializer, AddPropertySerializer
 
 class PropertyAPI(generics.RetrieveAPIView):
@@ -45,6 +46,36 @@ class GetOwnerPropertyAPI(generics.GenericAPIView):
         resp = {}
         i = 1
         for prop in properties:
+            resp[str(i)] = PropertySerializer(prop, context=self.get_serializer_context()).data
+            i += 1
+        return Response(resp)
+
+class GetSearchResultsAPI(generics.GenericAPIView):
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
+
+    #def get_queryset(self):
+
+    def get(self, request):
+        results = Property.objects.all()
+        suburb = request.GET.get('suburb')
+        if suburb != None :
+            results = results.filter(suburb = suburb)
+        post_code = request.GET.get("post_code")
+        if  post_code != None :
+            results = results.filter(postcode = post_code)
+        price = request.GET.get("price")
+        if price != None :
+            results = results.filter(price__lte = price)
+        start_date = request.GET.get('start_date')
+        end_date = request.GET.get('end_date')
+        if start_date != None and end_date!= None:
+            propertiesNotAvaliable = Booking.objects.filter(checkin__lte = end_date, checkout__gte = start_date).values_list('property_id', flat=True).distinct()
+            results = results.exclude(id__in = propertiesNotAvaliable)
+
+        resp = {}
+        i = 1
+        for prop in results:
             resp[str(i)] = PropertySerializer(prop, context=self.get_serializer_context()).data
             i += 1
         return Response(resp)
--- accomodation_app/property/urls.py
+++ accomodation_app/property/urls.py
@@ -1,11 +1,18 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls import url
-from .api import PropertyAPI, AddPropertyAPI, UpdatePropertyAPI, GetOwnerPropertyAPI
+from .api import PropertyAPI, AddPropertyAPI, UpdatePropertyAPI, GetOwnerPropertyAPI, GetSearchResultsAPI
 
 
 urlpatterns = [
     path('api/property/add', AddPropertyAPI.as_view()),
     url(r'^api/property/(?P<id>\d+)$', PropertyAPI.as_view(), name='Property'),
     url(r'^api/property/update/(?P<id>\d+)$', UpdatePropertyAPI.as_view()),
-    url(r'^api/property/owner/(?P<owner_id>\d+)$', GetOwnerPropertyAPI.as_view())
+    url(r'^api/property/owner/(?P<owner_id>\d+)$', GetOwnerPropertyAPI.as_view()),
+    re_path(r'^api/property/search/$', GetSearchResultsAPI.as_view())
+    #re_path(r'^api/property/search/(?P<no_beds>\d+)$/(?P<no_guests>\d+)$/(?P<no_bathrooms>\d+)$', GetSearchResultsAPI.as_view())
+    #re_path
+
+    #       ( '^        (?P<first_name>[a-zA-Z]+)/(?P<last_name>[a-zA-Z]+)(?:/(?P<title>[a-zA-Z]+))?/$','some_method'),
+    #       ( '^    (?:/(?P<title>[a-zA-Z]+))?/$','some_method'),
+    #re_path(r'^view(?:/(?P<dummy>[a-zA-Z]+))?(?:/(?P<dummy2>[a-zA-Z]+))?(?:/(?P<dummy3>[a-zA-Z]+))?/$', views.MyView.as_view(), name='myname'),
 ]
